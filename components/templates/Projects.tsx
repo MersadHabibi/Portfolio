4a67814dfb461a1d26162e855c91b80d487db878
mersad habibi <mersad.up@gmail.com>
2025-10-18 10:09:00 +0330

    Add sample project data for Tailwind CSS class generation in Projects component

--- components/templates/Projects.tsx
+++ components/templates/Projects.tsx
@@ -5,6 +5,65 @@ import Link from "next/link";
 import SectionHeader from "../modules/SectionHeader";
 import { projects } from "@/data/projects";
 
+// for tailwind to create classes on build time
+const fakeProjectsToCreateTailwindClasses = [
+  {
+    id: 1,
+    title: "Azarmelk",
+    image: "/images/projects/azarmelk.png",
+    buttonStyle:
+      "bg-second-color-200 border-[2.5px] border-second-color-300 shadow-lg shadow-second-color-100/40",
+    href: "https://azarmelek.com",
+    stack: "Front-End",
+    stackClassName:
+      "bg-second-color-100 border-2 border-second-color-200 shadow-lg shadow-second-color-100/40 text-second-color-300",
+  },
+  {
+    id: 2,
+    title: "Resistance Art",
+    image: "/images/projects/resistanceart.png",
+    buttonStyle:
+      "bg-second-color-200 border-[2.5px] border-second-color-300 shadow-lg shadow-second-color-100/40",
+    href: "https://resistanceart.ir",
+    stack: "Front-End",
+    stackClassName:
+      "bg-third-color-100 border-2 border-third-color-200 shadow-lg shadow-third-color-100/40 text-third-color-300",
+  },
+  {
+    id: 3,
+    title: "Next-Ecommerce",
+    image: "/images/projects/next-ecommerce.png",
+    buttonStyle:
+      "bg-first-color-200 border-[2.5px] border-first-color-300 shadow-lg shadow-first-color-100/40",
+    href: "https://next-ecommerce-murex-mu.vercel.app",
+    stack: "Full-Stack",
+    stackClassName:
+      "bg-first-color-100 border-2 border-first-color-200 shadow-lg shadow-first-color-100/40 text-first-color-300",
+  },
+  {
+    id: 4,
+    title: "Arya Shop",
+    image: "/images/projects/arya-shop.png",
+    buttonStyle:
+      "bg-fourth-color-200 border-[2.5px] border-fourth-color-300 shadow-lg shadow-fourth-color-100/40",
+    href: "https://arya-company.com/",
+    stack: "Front-End",
+    stackClassName:
+      "bg-fourth-color-100 border-2 border-fourth-color-200 shadow-lg shadow-fourth-color-100/40 text-fourth-color-300",
+  },
+  {
+    id: 5,
+    title: "Wiki Sefaresh",
+    image: "/images/projects/wiki-sefaresh.png",
+    buttonStyle:
+      "bg-third-color-200 border-[2.5px] border-third-color-300 shadow-lg shadow-third-color-100/40",
+    href: "https://www.wiki-sefaresh.ir",
+    stack: "Full-Stack",
+    stackClassName:
+      "bg-third-color-100 border-2 border-third-color-200 shadow-lg shadow-third-color-100/40 text-third-color-300",
+  },
+];
+
 export default function Projects() {
   return (
     <section className="flex flex-col px-3 pb-16 lg:flex-row lg:pb-24">
